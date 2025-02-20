829fbe3e708258e4eed178c6813641d4ded2a56a
IoT <IoT@DESKTOP-2C81TOO>
2025-02-20 14:58:57 +0200

    update

---
+++ HomeWork/ConfigurationBuildreExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace HomeWork
+{
+    public static class ConfigurationBuilderExtensions
+    {
+        public static IConfigurationBuilder Configure<T>(this IConfigurationBuilder configurationBuilder,
+            string basePath, bool addUserSecrets = true) where T : class
+        {
+            configurationBuilder
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("secrets/appsettings.secrets.json", optional: true)
+                .AddEnvironmentVariables();
+
+            if (addUserSecrets)
+            {
+                configurationBuilder.AddUserSecrets<T>();
+            }
+
+            return configurationBuilder;
+        }
+    }
+}
